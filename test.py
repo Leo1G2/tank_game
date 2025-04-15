26911c29c1667bfb2cf497d130b186157ce694d3
Leo1G2 <146435842+Leo1G2@users.noreply.github.com>
2025-04-15 14:18:02 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,20 @@
 import pygame
 
-# Initialisation de Pygame
+
 pygame.init()
 
-# Création de la fenêtre de jeu
+
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Tank Game")
 
 running = True
 while running:
-    # Gestion des événements
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # Remplir l'écran avec une couleur (noir)
+  
     screen.fill((32, 49, 203))
     pygame.display.flip()
 
